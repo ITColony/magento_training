0bd0261974481402961e141fdc6581b41084db59
ITColony <45891230+ITColony@users.noreply.github.com>
2018-12-15 09:00:52 +0100

    Add files via upload

---
+++ Arch Basics 09/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 12/13/2018
+ * Time: 8:09 AM
+ */
+
+namespace SimplifiedMagento\FirstModule\Controller\Page;
+
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use SimplifiedMagento\FirstModule\NotMagento\PencilInterface;
+
+class HelloWorld extends \Magento\Framework\App\Action\Action
+{
+    protected $pencilInterface;
+    public function __construct(Context $context,
+                                PencilInterface $pencilInterface
+                                )
+    {
+
+        $this->pencilInterface = $pencilInterface;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        echo $this->pencilInterface->getPencilType();
+    }
+}
+
+
